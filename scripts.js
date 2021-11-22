478439759ea370c20dec6074c755e74c265eab6a
JarlTetrazepam <gaterzoom3638@gmail.com>
2021-11-22 16:56:49 +0100

    fixed foxes not working

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 const fetchImage = () => {
     let APIs = ["https://thatcopy.pw/catapi/rest/", "https://random.dog/woof.json", "https://random-d.uk/api/random", "https://randomfox.ca/floof/"];
     let randomValue = Math.floor((Math.random() * APIs.length))
+    console.log('trying to access ' + APIs[randomValue])
     let oReq = new XMLHttpRequest();
     oReq.addEventListener("load", imageReplacer);
     oReq.open("GET", APIs[randomValue])
@@ -8,9 +9,9 @@ const fetchImage = () => {
 }
 
 function imageReplacer() {
-    console.log(JSON.parse(this.responseText)["url"])
+    console.log(JSON.parse(this.responseText)["url"] || JSON.parse(this.responseText)["image"])
     let imageTag = document.querySelector(".cuteImage")
-    let image = JSON.parse(this.responseText)["url"];
+    let image = JSON.parse(this.responseText)["url"] || JSON.parse(this.responseText)["image"];
     imageTag.src = image
 }
 
