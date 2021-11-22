adf93d33a1bee97b103f518d58097c216a5f1c2b
JarlTetrazepam <gaterzoom3638@gmail.com>
2021-11-22 16:52:15 +0100

    multiple sources working somewhat reliably

--- scripts.js
+++ scripts.js
@@ -1,13 +1,16 @@
 const fetchImage = () => {
+    let APIs = ["https://thatcopy.pw/catapi/rest/", "https://random.dog/woof.json", "https://random-d.uk/api/random", "https://randomfox.ca/floof/"];
+    let randomValue = Math.floor((Math.random() * APIs.length))
     let oReq = new XMLHttpRequest();
     oReq.addEventListener("load", imageReplacer);
-    oReq.open("GET", "https://thatcopy.pw/catapi/rest/")
+    oReq.open("GET", APIs[randomValue])
     oReq.send()
 }
 
 function imageReplacer() {
+    console.log(JSON.parse(this.responseText)["url"])
     let imageTag = document.querySelector(".cuteImage")
-    let image = this.responseText.split('"')[5];
+    let image = JSON.parse(this.responseText)["url"];
     imageTag.src = image
 }
 
